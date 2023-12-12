0d8bf6ba5fc34cb87639b2a21c89fd7ba7be92ce
Jarek Radosz <jradosz@gmail.com>
2023-12-12 17:25:59 +0100

    DEV: Update linting

--- assets/javascripts/discourse/components/anonymous-moderator-tab.js
+++ assets/javascripts/discourse/components/anonymous-moderator-tab.js
@@ -1,8 +1,8 @@
 import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
-import { action } from "@ember/object";
-import { ajax } from "discourse/lib/ajax";
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { ajax } from "discourse/lib/ajax";
 
 export default class AnonymousModeratorTab extends Component {
   @service currentUser;
