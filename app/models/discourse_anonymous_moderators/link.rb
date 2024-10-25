7f65e69d0e8e9662a11008482c6916a4ece52d4a
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-10-25 05:50:33 -0300

    DEV: Update plugin annotations (#49)

--- app/models/discourse_anonymous_moderators/link.rb
+++ app/models/discourse_anonymous_moderators/link.rb
@@ -9,18 +9,22 @@ end
 
 # == Schema Information
 #
-# Table name: anonymous_user_links
+# Table name: discourse_anonymous_moderators_links
 #
-#  id             :bigint(8)        not null, primary key
-#  user_id        :bigint(8)        not null
-#  parent_user_id :bigint(8)        not null
+#  id             :bigint           not null, primary key
+#  user_id        :bigint           not null
+#  parent_user_id :bigint           not null
 #  last_used_at   :datetime         not null
 #  created_at     :datetime         not null
 #  deactivated_at :datetime
 #
 # Indexes
 #
-#  index_anonymous_user_links_on_parent_user_id              (parent_user_id)
-#  index_anonymous_user_links_on_user_id                     (user_id)
-#  index_anonymous_user_links_on_user_id_and_parent_user_id  (user_id,parent_user_id) UNIQUE WHERE (deactivated_at IS NULL)
+#  index_discourse_anonymous_moderators_links_on_parent_user_id  (parent_user_id) UNIQUE WHERE (deactivated_at IS NULL)
+#  index_discourse_anonymous_moderators_links_on_user_id         (user_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (parent_user_id => users.id) ON DELETE => cascade
+#  fk_rails_...  (user_id => users.id) ON DELETE => cascade
 #
