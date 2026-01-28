e138bf654ad0bf5388c7fccb5492661dcddc3d01
Jarek Radosz <jradosz@gmail.com>
2026-01-28 12:47:45 +0100

    DEV: Update linting and add types (#75)
    * DEV: Update linting and add types
    
    * DEV: Update linting and add types
    
    * DEV: Update linting and add types
    
    * DEV: Update linting and add types

--- spec/lib/manager_spec.rb
+++ spec/lib/manager_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe DiscourseAnonymousModerators::Manager do
   let(:parent1) { Fabricate(:user) }
   let(:anon1) { Fabricate(:user) }
--- spec/requests/switch_controller_spec.rb
+++ spec/requests/switch_controller_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 RSpec.describe DiscourseAnonymousModerators::SwitchController do
   before do
     SiteSetting.anonymous_moderators_enabled = true
